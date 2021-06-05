c4ceee09668b0b9006502702b76a81d2285a5c11
bagastri07 <bagaskuhaku@gmail.com>
2021-06-05 10:42:58 +0700

    semua kata bisa

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,13 +1,12 @@
 import string
 
-sentence = 'abah uma wadah'
+sentence = 'abah uma wadah kupiah mauiah andak lajak kulahai baisi banyu'
 inputString = sentence.lower()+'#'
 
 alphabetList = list(string.ascii_lowercase)
 stateList = ['q0', 'q1', 'q2',  'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10',
             'q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17', 'q18', 'q19', 'q20',
-            'q21', 'q22', 'q23', 'q24', 'q25', 'q26', 'q27', 'q28', 'q29', 'q30',
-            'q31']
+            'q21', 'q22', 'q23', 'q24', 'q25', 'q26', 'q27', 'q28', 'q29', 'q30']
 
 transitionTable = {}
 
@@ -34,8 +33,48 @@ transitionTable[('q5', 'a')] = 'q6'
 transitionTable[('q0', 'w')] = 'q8'
 transitionTable[('q8', 'a')] = 'q9'
 transitionTable[('q9', 'd')] = 'q2'
-transitionTable[('q2', 'a')] = 'q3'
-transitionTable[('q3', 'h')] = 'q6'
+
+#kupiah
+transitionTable[('q0', 'k')] = 'q10'
+transitionTable[('q10', 'u')] = 'q11'
+transitionTable[('q11', 'p')] = 'q12'
+transitionTable[('q12', 'i')] = 'q2'
+
+#mauiah
+transitionTable[('q0', 'm')] = 'q13'
+transitionTable[('q13', 'a')] = 'q14'
+transitionTable[('q14', 'u')] = 'q15'
+transitionTable[('q15', 'i')] = 'q2'
+
+#andak
+transitionTable[('q1', 'n')] = 'q16'
+transitionTable[('q16', 'd')] = 'q17'
+transitionTable[('q17', 'a')] = 'q18'
+transitionTable[('q18', 'k')] = 'q6'
+
+#lajak
+transitionTable[('q0', 'l')] = 'q19'
+transitionTable[('q19', 'a')] = 'q20'
+transitionTable[('q20', 'j')] = 'q17'
+
+#kulahai
+transitionTable[('q11', 'l')] = 'q21'
+transitionTable[('q21', 'a')] = 'q22'
+transitionTable[('q22', 'h')] = 'q23'
+transitionTable[('q23', 'a')] = 'q24'
+transitionTable[('q24', 'i')] = 'q6'
+
+#baisi
+transitionTable[('q0', 'b')] = 'q25'
+transitionTable[('q25', 'a')] = 'q26'
+transitionTable[('q26', 'i')] = 'q27'
+transitionTable[('q27', 's')] = 'q28'
+transitionTable[('q28', 'i')] = 'q6'
+
+#banyu
+transitionTable[('q26', 'n')] = 'q29'
+transitionTable[('q29', 'y')] = 'q30'
+transitionTable[('q30', 'u')] = 'q6'
 
 #acceptable
 transitionTable[('q6', ' ')] = 'q7'
@@ -47,8 +86,10 @@ transitionTable[('q7', '#')] = 'accept'
 transitionTable[('q7', 'a')] = 'q1'
 transitionTable[('q7', 'u')] = 'q4'
 transitionTable[('q7', 'w')] = 'q8'
-
-
+transitionTable[('q7', 'k')] = 'q10'
+transitionTable[('q7', 'm')] = 'q13'
+transitionTable[('q7', 'l')] = 'q19'
+transitionTable[('q7', 'b')] = 'q25'
 
 
 
